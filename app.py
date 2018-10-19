b702e787b95c6af602b3feff6642565d4feaeb1b
Jonny Kalambay <jonnyk20@gmail.com>
2018-10-20 07:45:37 +0800

    Removing unused route in app.py

--- app.py
+++ app.py
@@ -5,20 +5,9 @@ from time import sleep
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/json')
-def json():
-    dictionary = {
-        "Key1": "one",
-        "Key2": 2
-    }
-    return jsonify(dictionary)
-
 
 @app.route('/move', methods=['POST'])
 def move():
@@ -26,7 +15,6 @@ def move():
     json = request.get_json()
     moveDirection(json)
     return "Move request received"
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
